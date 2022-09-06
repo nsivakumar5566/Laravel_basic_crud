18ddbb26dd37779103b17d1212e5fd1d5e7deb65
sivakumar <sivakumar.n@synamen.com>
2022-09-06 13:06:03 +0530

    validation

--- resources/views/postview/create.blade.php
+++ resources/views/postview/create.blade.php
@@ -11,24 +11,29 @@
           <label for="name" class="form-label">Name</label>
           <input type="text" class="form-control" id="name" name="name">
           @if ($errors->has('name'))
-          <div class="invalid-feedback">
-            {{ $errors->first('name') }}
-          </div>
+          
+            <span class="error" style="color:red;">{{ $errors->first('name') }}</span>
+          
           @endif
         </div>
         <div class="mb-3">
           <label for="desc" class="form-label">Description</label>
           <input type="text" class="form-control" id="desc" name="description">
           @if ($errors->has('description'))
-          <div class="invalid-feedback">
-            {{ $errors->first('description') }}
-          </div>
+        
+          <span class="error" style="color:red;">{{ $errors->first('description') }}</span>
+         
           @endif
         </div>
 
         <div class="mb-3">
           <label for="desc" class="form-label">Images</label>
           <input type="file" class="form-control"  name="image">
+          @if ($errors->has('image'))
+        
+        <span class="error" style="color:red;">{{ $errors->first('image') }}</span>
+       
+        @endif
         </div>
         <div class="mb-3">
         <label for="desc" class="form-label">Ages</label>
@@ -38,6 +43,11 @@
           <option value="Two">Two</option>
           <option value="Three">Three</option>
         </select>
+        @if ($errors->has('age'))
+        
+        <span class="error" style="color:red;">{{ $errors->first('age') }}</span>
+       
+        @endif
        </div>
         <button type="submit" class="btn btn-primary w-100">Create Post</button>
       </form>
